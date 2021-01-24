bc2be856e80df867c1ba9efbad28b4773772b384
Carlos <cdriangt@gmail.com>
2021-01-24 16:54:28 -0400

    i forgot this padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Header extends Component {
      render() {
           return (
-               <header id="welcome-section" className="h-screen flex flex-col pl-6 sm:pl-0 sm:items-center sm:text-center bg-gradient-to-b from-coolGray-100 via-coolGray-200 to-coolGray-300">
+               <header id="welcome-section" className="h-screen flex flex-col py-6 sm:py-0 sm:items-center sm:text-center bg-gradient-to-b from-coolGray-100 via-coolGray-200 to-coolGray-300">
                     <h1 className="h-1/2 text-7xl sm:text-8xl font-gogh text-indigo-900 flex items-end">I'm Carlos Guzman</h1>
                     <div className="h-1/2 text-xl text-coolGray-700 flex flex-col">
                          <p className="h-3/5">An electrical engineer who started to learn web development</p>
