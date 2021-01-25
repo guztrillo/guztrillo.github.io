1400dcd182de7b42d8ad27778e7e400a69c70726
Carlos <cdriangt@gmail.com>
2021-01-25 15:56:04 -0400

    padding bottom

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
                     <h1 className="h-1/2 text-7xl sm:text-8xl font-gogh text-indigo-900 flex items-end">I'm Carlos Guzman</h1>
                     <div className="h-1/2 text-xl text-coolGray-700 flex flex-col">
                          <p className="h-3/5">An electrical engineer who started to learn web development</p>
-                         <p className='h-2/5 text-base text-indigo-400 flex items-center sm:justify-center'>This website was created using React.js and TailWindCSS.</p>
+                         <p className='h-2/5 text-base text-indigo-400 flex items-center sm:justify-center pb-20 sm:pb-0'>This website was created using React.js and TailWindCSS.</p>
                     </div>
                </header>
           )
