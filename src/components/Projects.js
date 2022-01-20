f81bfb6486e6498ada7a8dda890bf643f06e54f8
Carlos <cdriangt@gmail.com>
2022-01-20 17:12:18 -0400

    change projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import survey from '../img/carlosguzman-form.png'
 import calculator from '../img/javascript-calculator.png'
-import product from '../img/lading-page.png'
 import drum from '../img/drum-machine.png'
 import quote from '../img/quote-machine.png'
 import pomodoro from '../img/pomodoro-clock.png'
+import cards from '../img/cards.png';
+import faq from '../img/faq.png'
 
 export default class Projects extends Component {
      render() {
@@ -14,6 +14,16 @@ export default class Projects extends Component {
                          <h2>Some of my works so far</h2>
                     </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 m-10 font-bold">
+                         <div className="group card">
+                              <a href={'https://guztrillo.github.io/TFM-FAQ-accordion-card/'} target="_blank" rel="noopener noreferrer" className="link">
+                                   <div className="container-pic">
+                                        <img src={faq} alt=" My survey form page" className="picture" />
+                                   </div>
+                                   <div className="title">
+                                        <h3>FAQ accordion card</h3>
+                                   </div>
+                              </a>
+                         </div>
                          <div className="card">
                               <a href={'https://guztrillo.github.io/javascript-calculator/'} target="_blank" rel="noopener noreferrer" className="group">
                                    <div className="container-pic">
@@ -45,32 +55,22 @@ export default class Projects extends Component {
                               </a>
                          </div>
                          <div className="group card">
+                              <a href={'https://guztrillo.github.io/TFM-four-card-feature-section-master/'} target="_blank" rel="noopener noreferrer" className="link">
+                                   <div className="container-pic">
+                                        <img src={cards} alt="My product landing page" className="picture" />
+                                   </div>
+                                   <div className="title">
+                                        <h3>Four card feature section</h3>
+                                   </div>
+                              </a>
+                         </div>
+                         <div className="group card">
                               <a href={'https://guztrillo.github.io/drum-machine/'} target="_blank" rel="noopener noreferrer" className="link">
                                    <div className="container-pic">
                                         <img src={drum} alt=" My drum machine" className="picture" />
                                    </div>
                                    <div className="title">
                                         <h3>Drum Machine</h3>
-                                   </div>
-                              </a>
-                         </div>
-                         <div className="group card">
-                              <a href={'https://guztrillo.github.io/survey-form/'} target="_blank" rel="noopener noreferrer" className="link">
-                                   <div className="container-pic">
-                                        <img src={survey} alt=" My survey form page" className="picture" />
-                                   </div>
-                                   <div className="title">
-                                        <h3>Survey Form</h3>
-                                   </div>
-                              </a>
-                         </div>
-                         <div className="group card">
-                              <a href={'https://guztrillo.github.io/product-landing-page/'} target="_blank" rel="noopener noreferrer" className="link">
-                                   <div className="container-pic">
-                                        <img src={product} alt="My product landing page" className="picture" />
-                                   </div>
-                                   <div className="title">
-                                        <h3>Product Landing Page</h3>
                                    </div>
                               </a>
                          </div>
